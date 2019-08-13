064cbd392e9640c5e0f713a96a6cac8e76c5b3b4
adriancr93 <aobregonr@ucenfotec.ac.cr>
2019-08-12 23:15:25 -0600

    Add Proyect in DataBases
    Me falta mostrar proyectos cargados

---
+++ src/main/java/com/patterns/entity/ControllerProyect.java
@@ -0,0 +1,52 @@
+package main.java.com.patterns.entity;
+
+import java.io.IOException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ControllerProyect")
+public class ControllerProyect extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String nombreProyecto = request.getParameter("nombreProyecto");
+		String idEquipo = request.getParameter("idEquipo");
+		String descripcion = request.getParameter("descripcion");
+		boolean result = true;
+		
+		if(nombreProyecto.isEmpty() || idEquipo.isEmpty() || descripcion.isEmpty())
+		{
+			RequestDispatcher req = request.getRequestDispatcher("login.jsp");
+			req.include(request, response);
+		} else {
+			try {
+				Connection conn = DBConexion.Connect();
+				String query = "INSERT INTO proyect "
+						+ "(nameProyect, teamAsing, description)"
+						+" VALUES (?, ?, ?);";
+				
+				PreparedStatement statement = conn.prepareStatement(query);
+				statement.setString(1, nombreProyecto);
+				statement.setInt(2, Integer.parseInt(idEquipo));
+				statement.setString(3, descripcion);
+
+				result = statement.execute();
+				DBConexion.Disconnect();
+			}catch(SQLException ex) {
+				System.out.println(ex.getMessage());
+			}
+	    }
+		
+	}
+
+}
